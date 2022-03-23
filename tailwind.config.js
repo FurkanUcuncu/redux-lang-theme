59b4c554411ccd2a30d5b4399d254dcf6267783f
FurkanUcuncu <73463054+FurkanUcuncu@users.noreply.github.com>
2022-03-23 17:17:44 +0300

    Theme colors

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  important: true,
+  theme: {
+    extend: {
+      colors: {
+        'customGray-500' : "#1E1D1E",
+        'customGray-400' : "#2A2D35",
+        'customGray-300' : "#31343E",
+        'customGray-200' : "#ffffff0d",
+        'customGray-100': "#ffffffb3",
+
+        primary: "#0000008a",
+        secondary: "#f5f5f5",
+      },
+    },
+  },
+  plugins: [],
+}
